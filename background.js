ebf6edd8968b42d0ecf3eca14e519082239b9084
Serena X <84420380+xuserena12@users.noreply.github.com>
2024-08-03 23:11:23 -0400

    screenshot

--- background.js
+++ background.js
@@ -5,5 +5,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         console.log('Mouse down at: ', message.x, message.y);
     } else if (message.event === 'mouseup') {
         console.log('Mouse up at: ', message.x, message.y);
+    } else if (message.event === 'captureRequest') {
+        chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUrl) {
+            if (chrome.runtime.lastError) {
+                console.error('Error capturing screen:', chrome.runtime.lastError.message);
+            } else {
+                console.log('Screenshot is taken at:', dataUrl);
+            }
+        });   
     }
 });
